54806a52f50e09e220381904b0586101c9c8bc85
UwU <maulenadasmejose@gmail.com>
2021-04-20 20:14:34 -0400

    el amanecer funciona pero hay que optimizarlo

--- dayTime.cs
+++ dayTime.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class dayTime : MonoBehaviour
 {
     public float speed;
-    public float intensity;
+    public float intsty;
     Light myL;
+    public Light ambientL;
+    public float ambientY;
+
 void Start(){
     myL=GetComponent<Light>();
 }
 void FixedUpdate(){
-    myL.intensity=intensity; 
+    Debug.Log(transform.localRotation.x);
     Vector3 rotor= new Vector3(speed,0,0);
     transform.Rotate(rotor);
-}
+    if(transform.localRotation.x>=-0.09f){
+        myL.intensity=intsty;
+        ambientL.intensity=ambientY; 
+        speed=0.008f;
+        if (intsty<=1.2f){
+            intsty=1.2f;
+        }else
+        {
+        intsty-=0.008f;
+        ambientL.color+=(Color.white*1)*speed;
+        }
+    }
+    if(transform.localRotation.x>=0.7f)
+    {
+        transform.localEulerAngles= new Vector3(-260,0,0);
+        speed=0.4f;
+        intsty=5f;
+        ambientY=0.7f;
+    }
+  }
 }
--- menucamera.cs
+++ menucamera.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class menucamera : MonoBehaviour
 {
     float speed=1;
+    public bool xFollow;
+    public bool yFollow;
+    public bool zFollow;
+
     float yoffset;
     public float ypos;
     public GameObject target;
@@ -12,9 +16,19 @@ public class menucamera : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     { 
+        if (yFollow==true)
+        {
         yoffset=target.transform.position.y;        
         speed=target.transform.position.x; 
-        transform.position= new Vector3(speed,ypos+yoffset*0.2f,transform.position.z);
+        transform.position= new Vector3(speed,ypos+yoffset*0.2f,transform.position.z);  
+        }
+        else{
+        yoffset=target.transform.position.y;        
+        speed=target.transform.position.x; 
+        transform.position= new Vector3(target.transform.position.x,transform.position.y,transform.position.z);  
+
+        }
 
     }
 }
+ 
