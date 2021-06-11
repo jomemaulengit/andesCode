42712f3cd885907c753713515cce0e87136c0689
UwU <maulenadasmejose@gmail.com>
2021-06-10 21:56:20 -0400

    intro[taptoplay-NOT-50%],dayTime[rain-NOT],trbehvior[limit-NOT-90%],BGcotrol[spawn-NOT-90%],camera[zoom-NOT,lerp-NOT-30%]

--- intro.cs
+++ intro.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class intro : MonoBehaviour
 {
     // =========================PUBLIC VARIABLES===================================
+    public bool introIsOver=false;
     public new transformBehavior camera;
     public Image image;
+    public Sprite title;
     public Text text;
     public float yoffsetSpeed;
     public float fadeRatio;
@@ -21,13 +23,24 @@ public class intro : MonoBehaviour
     IEnumerator FadeIn(){
         for(float f =0.04f; f<=1f; f+=fadeRatio){
             image.color=new Color(255,255,255,f);
+            if(image.color.a>=0.9f && introIsOver==false){
+                StartCoroutine("FadeOut");
+                StopCoroutine("FadeIn");
+            }  
             yield return new WaitForSeconds(0.01f);
         }
     }
     IEnumerator FadeOut(){
         for(float fo =0.9f; fo>0f; fo-=fadeRatio){
             image.color=new Color(255,255,255,fo);
-            yield return new WaitForSeconds(0.01f);
+            if(image.color.a<=0.02f){
+                StartCoroutine("FadeIn");
+                image.sprite=title;
+                fadeRatio=0.008f;
+                introIsOver=true;
+                StopCoroutine("FadeOut");
+            }             
+            yield return new WaitForSeconds(0.007f);
         }
     }
     void Start()
@@ -38,15 +51,11 @@ public class intro : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-     if(image.color.a>=0.9f){
-         StopCoroutine("FadeIn");
-         StartCoroutine("FadeOut");
-     }   
+    { 
      if(camera.yOff>=-0.0001f){
          StopCoroutine("IntroPos");
          camera.yOff=0f;
-        //  this.enabled=false;
+         text.color= new Color(0,0,0,text.color.a+0.06f);
      }
     }
 }
